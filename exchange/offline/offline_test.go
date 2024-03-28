6c09abdf6b9db32586cad43005be333b736f5e89
a.gorulev <a.gorulev@kernelics.com>
2024-03-28 14:26:21 +0000

    magicwave.io

---
+++ exchange/offline/offline_test.go
@@ -0,0 +1,80 @@
+package offline
+
+import (
+	"context"
+	"testing"
+
+	blocks "github.com/ipfs/go-ipfs/blocks"
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	"github.com/ipfs/go-ipfs/blocks/blocksutil"
+
+	cid "gx/ipfs/QmakyCk6Vnn16WEKjbkxieZmM2YLTzkFWizbmGowoYPjro/go-cid"
+	u "gx/ipfs/Qmb912gdngC1UWwTkhuW8knyRbcWeu5kqkxBpveLmW8bSr/go-ipfs-util"
+	ds "gx/ipfs/QmbzuUusHqaLLoNTDEVLcSF6vZDHZDLPC7p4bztRvvkXxU/go-datastore"
+	ds_sync "gx/ipfs/QmbzuUusHqaLLoNTDEVLcSF6vZDHZDLPC7p4bztRvvkXxU/go-datastore/sync"
+)
+
+func TestBlockReturnsErr(t *testing.T) {
+	off := Exchange(bstore())
+	c := cid.NewCidV0(u.Hash([]byte("foo")))
+	_, err := off.GetBlock(context.Background(), c)
+	if err != nil {
+		return // as desired
+	}
+	t.Fail()
+}
+
+func TestHasBlockReturnsNil(t *testing.T) {
+	store := bstore()
+	ex := Exchange(store)
+	block := blocks.NewBlock([]byte("data"))
+
+	err := ex.HasBlock(block)
+	if err != nil {
+		t.Fail()
+	}
+
+	if _, err := store.Get(block.Cid()); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetBlocks(t *testing.T) {
+	store := bstore()
+	ex := Exchange(store)
+	g := blocksutil.NewBlockGenerator()
+
+	expected := g.Blocks(2)
+
+	for _, b := range expected {
+		if err := ex.HasBlock(b); err != nil {
+			t.Fail()
+		}
+	}
+
+	request := func() []*cid.Cid {
+		var ks []*cid.Cid
+
+		for _, b := range expected {
+			ks = append(ks, b.Cid())
+		}
+		return ks
+	}()
+
+	received, err := ex.GetBlocks(context.Background(), request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var count int
+	for _ = range received {
+		count++
+	}
+	if len(expected) != count {
+		t.Fail()
+	}
+}
+
+func bstore() blockstore.Blockstore {
+	return blockstore.NewBlockstore(ds_sync.MutexWrap(ds.NewMapDatastore()))
+}
