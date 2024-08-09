5209450eaca67e57189ea4bc23f6e841ac6118a0
a.gorulev <a.gorulev@kernelics.com>
2024-08-09 17:28:20 +0000

    magicwave.io

---
+++ exchange/bitswap/network/interface.go
@@ -0,0 +1,60 @@
+package network
+
+import (
+	"context"
+
+	bsmsg "github.com/ipfs/go-ipfs/exchange/bitswap/message"
+	protocol "gx/ipfs/QmZNkThpqfVXs9GNbexPrfBbXSLNYeKrE7jwFM2oqHbyqN/go-libp2p-protocol"
+	cid "gx/ipfs/QmakyCk6Vnn16WEKjbkxieZmM2YLTzkFWizbmGowoYPjro/go-cid"
+	peer "gx/ipfs/QmfMmLGoKzCHDN7cGgk64PJr4iipzidDRME8HABSJqvmhC/go-libp2p-peer"
+)
+
+var ProtocolBitswap protocol.ID = "/ipfs/bitswap/1.0.0"
+var ProtocolBitswapOld protocol.ID = "/ipfs/bitswap"
+
+// BitSwapNetwork provides network connectivity for BitSwap sessions
+type BitSwapNetwork interface {
+
+	// SendMessage sends a BitSwap message to a peer.
+	SendMessage(
+		context.Context,
+		peer.ID,
+		bsmsg.BitSwapMessage) error
+
+	// SetDelegate registers the Reciver to handle messages received from the
+	// network.
+	SetDelegate(Receiver)
+
+	ConnectTo(context.Context, peer.ID) error
+
+	NewMessageSender(context.Context, peer.ID) (MessageSender, error)
+
+	Routing
+}
+
+type MessageSender interface {
+	SendMsg(bsmsg.BitSwapMessage) error
+	Close() error
+}
+
+// Implement Receiver to receive messages from the BitSwapNetwork
+type Receiver interface {
+	ReceiveMessage(
+		ctx context.Context,
+		sender peer.ID,
+		incoming bsmsg.BitSwapMessage)
+
+	ReceiveError(error)
+
+	// Connected/Disconnected warns bitswap about peer connections
+	PeerConnected(peer.ID)
+	PeerDisconnected(peer.ID)
+}
+
+type Routing interface {
+	// FindProvidersAsync returns a channel of providers for the given key
+	FindProvidersAsync(context.Context, *cid.Cid, int) <-chan peer.ID
+
+	// Provide provides the key to the network
+	Provide(context.Context, *cid.Cid) error
+}
