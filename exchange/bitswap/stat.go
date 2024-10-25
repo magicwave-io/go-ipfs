88b1503ae74c8922430a66e33dc2062fdc416646
a.gorulev <a.gorulev@kernelics.com>
2024-10-25 01:27:27 +0000

    magicwave.io

---
+++ exchange/bitswap/stat.go
@@ -0,0 +1,34 @@
+package bitswap
+
+import (
+	"sort"
+
+	cid "gx/ipfs/QmakyCk6Vnn16WEKjbkxieZmM2YLTzkFWizbmGowoYPjro/go-cid"
+)
+
+type Stat struct {
+	ProvideBufLen   int
+	Wantlist        []*cid.Cid
+	Peers           []string
+	BlocksReceived  int
+	DupBlksReceived int
+	DupDataReceived uint64
+}
+
+func (bs *Bitswap) Stat() (*Stat, error) {
+	st := new(Stat)
+	st.ProvideBufLen = len(bs.newBlocks)
+	st.Wantlist = bs.GetWantlist()
+	bs.counterLk.Lock()
+	st.BlocksReceived = bs.blocksRecvd
+	st.DupBlksReceived = bs.dupBlocksRecvd
+	st.DupDataReceived = bs.dupDataRecvd
+	bs.counterLk.Unlock()
+
+	for _, p := range bs.engine.Peers() {
+		st.Peers = append(st.Peers, p.Pretty())
+	}
+	sort.Strings(st.Peers)
+
+	return st, nil
+}
