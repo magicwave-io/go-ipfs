7b18d0f42db9b9c3e82c69ee3662614518d35a93
a.gorulev <a.gorulev@kernelics.com>
2024-12-19 23:28:08 +0000

    magicwave.io

---
+++ blockservice/blockservice.go
@@ -0,0 +1,188 @@
+// package blockservice implements a BlockService interface that provides
+// a single GetBlock/AddBlock interface that seamlessly retrieves data either
+// locally or from a remote peer through the exchange.
+package blockservice
+
+import (
+	"errors"
+	"fmt"
+
+	blocks "github.com/ipfs/go-ipfs/blocks"
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	exchange "github.com/ipfs/go-ipfs/exchange"
+
+	context "context"
+	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	cid "gx/ipfs/QmakyCk6Vnn16WEKjbkxieZmM2YLTzkFWizbmGowoYPjro/go-cid"
+)
+
+var log = logging.Logger("blockservice")
+
+var ErrNotFound = errors.New("blockservice: key not found")
+
+// BlockService is a hybrid block datastore. It stores data in a local
+// datastore and may retrieve data from a remote Exchange.
+// It uses an internal `datastore.Datastore` instance to store values.
+type BlockService struct {
+	// TODO don't expose underlying impl details
+	Blockstore blockstore.Blockstore
+	Exchange   exchange.Interface
+}
+
+// NewBlockService creates a BlockService with given datastore instance.
+func New(bs blockstore.Blockstore, rem exchange.Interface) *BlockService {
+	if rem == nil {
+		log.Warning("blockservice running in local (offline) mode.")
+	}
+
+	return &BlockService{
+		Blockstore: bs,
+		Exchange:   rem,
+	}
+}
+
+// AddBlock adds a particular block to the service, Putting it into the datastore.
+// TODO pass a context into this if the remote.HasBlock is going to remain here.
+func (s *BlockService) AddBlock(o blocks.Block) (*cid.Cid, error) {
+	// TODO: while this is a great optimization, we should think about the
+	// possibility of streaming writes directly to disk. If we can pass this object
+	// all the way down to the datastore without having to 'buffer' its data,
+	// we could implement a `WriteTo` method on it that could do a streaming write
+	// of the content, saving us (probably) considerable memory.
+	c := o.Cid()
+	has, err := s.Blockstore.Has(c)
+	if err != nil {
+		return nil, err
+	}
+
+	if has {
+		return c, nil
+	}
+
+	err = s.Blockstore.Put(o)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.Exchange.HasBlock(o); err != nil {
+		return nil, errors.New("blockservice is closed")
+	}
+
+	return c, nil
+}
+
+func (s *BlockService) AddBlocks(bs []blocks.Block) ([]*cid.Cid, error) {
+	var toput []blocks.Block
+	for _, b := range bs {
+		has, err := s.Blockstore.Has(b.Cid())
+		if err != nil {
+			return nil, err
+		}
+
+		if has {
+			continue
+		}
+
+		toput = append(toput, b)
+	}
+
+	err := s.Blockstore.PutMany(toput)
+	if err != nil {
+		return nil, err
+	}
+
+	var ks []*cid.Cid
+	for _, o := range toput {
+		if err := s.Exchange.HasBlock(o); err != nil {
+			return nil, fmt.Errorf("blockservice is closed (%s)", err)
+		}
+
+		ks = append(ks, o.Cid())
+	}
+	return ks, nil
+}
+
+// GetBlock retrieves a particular block from the service,
+// Getting it from the datastore using the key (hash).
+func (s *BlockService) GetBlock(ctx context.Context, c *cid.Cid) (blocks.Block, error) {
+	log.Debugf("BlockService GetBlock: '%s'", c)
+
+	block, err := s.Blockstore.Get(c)
+	if err == nil {
+		return block, nil
+	}
+
+	if err == blockstore.ErrNotFound && s.Exchange != nil {
+		// TODO be careful checking ErrNotFound. If the underlying
+		// implementation changes, this will break.
+		log.Debug("Blockservice: Searching bitswap")
+		blk, err := s.Exchange.GetBlock(ctx, c)
+		if err != nil {
+			if err == blockstore.ErrNotFound {
+				return nil, ErrNotFound
+			}
+			return nil, err
+		}
+		return blk, nil
+	}
+
+	log.Debug("Blockservice GetBlock: Not found")
+	if err == blockstore.ErrNotFound {
+		return nil, ErrNotFound
+	}
+
+	return nil, err
+}
+
+// GetBlocks gets a list of blocks asynchronously and returns through
+// the returned channel.
+// NB: No guarantees are made about order.
+func (s *BlockService) GetBlocks(ctx context.Context, ks []*cid.Cid) <-chan blocks.Block {
+	out := make(chan blocks.Block, 0)
+	go func() {
+		defer close(out)
+		var misses []*cid.Cid
+		for _, c := range ks {
+			hit, err := s.Blockstore.Get(c)
+			if err != nil {
+				misses = append(misses, c)
+				continue
+			}
+			log.Debug("Blockservice: Got data in datastore")
+			select {
+			case out <- hit:
+			case <-ctx.Done():
+				return
+			}
+		}
+
+		if len(misses) == 0 {
+			return
+		}
+
+		rblocks, err := s.Exchange.GetBlocks(ctx, misses)
+		if err != nil {
+			log.Debugf("Error with GetBlocks: %s", err)
+			return
+		}
+
+		for b := range rblocks {
+			select {
+			case out <- b:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
+}
+
+// DeleteBlock deletes a block in the blockservice from the datastore
+func (s *BlockService) DeleteBlock(o blocks.Block) error {
+	return s.Blockstore.DeleteBlock(o.Cid())
+}
+
+func (s *BlockService) Close() error {
+	log.Debug("blockservice is shutting down...")
+	return s.Exchange.Close()
+}
---
+++ merkledag/coding.go
@@ -0,0 +1,101 @@
+package merkledag
+
+import (
+	"fmt"
+	"sort"
+
+	pb "github.com/ipfs/go-ipfs/merkledag/pb"
+
+	mh "gx/ipfs/QmYDds3421prZgqKbLpEK7T9Aa2eVdQ7o3YarX1LVLdP2J/go-multihash"
+	cid "gx/ipfs/QmakyCk6Vnn16WEKjbkxieZmM2YLTzkFWizbmGowoYPjro/go-cid"
+	u "gx/ipfs/Qmb912gdngC1UWwTkhuW8knyRbcWeu5kqkxBpveLmW8bSr/go-ipfs-util"
+)
+
+// for now, we use a PBNode intermediate thing.
+// because native go objects are nice.
+
+// unmarshal decodes raw data into a *Node instance.
+// The conversion uses an intermediate PBNode.
+func (n *Node) unmarshal(encoded []byte) error {
+	var pbn pb.PBNode
+	if err := pbn.Unmarshal(encoded); err != nil {
+		return fmt.Errorf("Unmarshal failed. %v", err)
+	}
+
+	pbnl := pbn.GetLinks()
+	n.Links = make([]*Link, len(pbnl))
+	for i, l := range pbnl {
+		n.Links[i] = &Link{Name: l.GetName(), Size: l.GetTsize()}
+		h, err := mh.Cast(l.GetHash())
+		if err != nil {
+			return fmt.Errorf("Link hash #%d is not valid multihash. %v", i, err)
+		}
+		n.Links[i].Hash = h
+	}
+	sort.Stable(LinkSlice(n.Links)) // keep links sorted
+
+	n.data = pbn.GetData()
+	n.encoded = encoded
+	return nil
+}
+
+// Marshal encodes a *Node instance into a new byte slice.
+// The conversion uses an intermediate PBNode.
+func (n *Node) Marshal() ([]byte, error) {
+	pbn := n.getPBNode()
+	data, err := pbn.Marshal()
+	if err != nil {
+		return data, fmt.Errorf("Marshal failed. %v", err)
+	}
+	return data, nil
+}
+
+func (n *Node) getPBNode() *pb.PBNode {
+	pbn := &pb.PBNode{}
+	if len(n.Links) > 0 {
+		pbn.Links = make([]*pb.PBLink, len(n.Links))
+	}
+
+	sort.Stable(LinkSlice(n.Links)) // keep links sorted
+	for i, l := range n.Links {
+		pbn.Links[i] = &pb.PBLink{}
+		pbn.Links[i].Name = &l.Name
+		pbn.Links[i].Tsize = &l.Size
+		pbn.Links[i].Hash = []byte(l.Hash)
+	}
+
+	if len(n.data) > 0 {
+		pbn.Data = n.data
+	}
+	return pbn
+}
+
+// EncodeProtobuf returns the encoded raw data version of a Node instance.
+// It may use a cached encoded version, unless the force flag is given.
+func (n *Node) EncodeProtobuf(force bool) ([]byte, error) {
+	sort.Stable(LinkSlice(n.Links)) // keep links sorted
+	if n.encoded == nil || force {
+		n.cached = nil
+		var err error
+		n.encoded, err = n.Marshal()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if n.cached == nil {
+		n.cached = cid.NewCidV0(u.Hash(n.encoded))
+	}
+
+	return n.encoded, nil
+}
+
+// Decoded decodes raw data and returns a new Node instance.
+func DecodeProtobuf(encoded []byte) (*Node, error) {
+	n := new(Node)
+	err := n.unmarshal(encoded)
+	if err != nil {
+		return nil, fmt.Errorf("incorrectly formatted merkledag node: %s", err)
+	}
+	return n, nil
+}
